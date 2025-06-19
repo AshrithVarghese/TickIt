158b4736376cc0c0cd25a8f3487725939fcc2e88
Ashrith Varghese <asrithalappat@gmail.com>
2025-06-19 21:29:33 +0530

    Basic setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,10 @@
-const PORT = 3000;
+import express from "express"
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
